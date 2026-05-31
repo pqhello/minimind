a840e62382e9d78981b404edc98ca337b7a32f29
peiqi <20021028pq@gmail.com>
2026-05-31 22:55:26 +0800

    RMSnorm功能

--- model/model.py
+++ model/model.py
@@ -70,3 +70,27 @@ class MokioMindConfig(PretrainedConfig):
             if self.inference_rope_scaling
             else None
         )
+
+'''
+RMSnorm 的作用就是归一化,防止计算值过大或者过小导致数值不稳定,从而加速模型的训练和收敛.
+它通过计算输入的均值和标准差来进行归一化,并且引入了一个可学习的缩放参数来调整归一化后的输出.
+相比于 LayerNorm, RMSNorm 不需要计算均值,因此在某些情况下可以更高效地进行归一化.
+通常防止梯度爆炸和梯度消失,从而提高模型的训练稳定性和性能.
+'''
+#继承nn.Module类
+import torch
+import torch.nn as nn
+class RMSNorm(nn.Module):
+#__init__初始化
+    def __init__(self,dim:int,eps:float=1e-5):
+        super().__init__()
+        self.dim = dim
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+#norm
+    def _norm(self,x):
+        return torch.rsqrt(x.pow(2).mean(-1,keepdim=True)+self.eps)
+    
+#forward前向传播
+    def forward(self,x):
+        return x*self._norm(x.float()).type_as(x) * self.weight
